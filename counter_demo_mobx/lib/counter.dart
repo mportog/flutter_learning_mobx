d81451b018dffb71d78d324896c920f466fedc41
Porto <matheus.garcia@t-systems.com.br>
2020-04-19 14:35:20 -0300

    add annotations e codegen

--- counter_demo_mobx/lib/counter.dart
+++ counter_demo_mobx/lib/counter.dart
@@ -1,28 +1,25 @@
 import 'package:mobx/mobx.dart';
 
-class Counter {
-  Counter() {
-    increment = Action(_increment);
-    decrement = Action(_decrement);
-    reset = Action(_reset);
+part 'counter.g.dart';
+
+class Counter = _Counter with _$Counter;
+
+abstract class _Counter with Store {
+  @observable
+  int count = 0;
+
+  @action
+  void increment() {
+    count++;
   }
 
-  Observable _count = Observable(0);
-
-  int get count => _count.value;
-
-  Action increment;
-  void _increment() {
-    _count.value++;
+  @action
+  void decrement() {
+    count--;
   }
 
-  Action decrement;
-  void _decrement() {
-    _count.value--;
-  }
-
-  Action reset;
-  void _reset() {
-    _count.value = 0;
+  @action
+  void reset() {
+    count = 0;
   }
 }
